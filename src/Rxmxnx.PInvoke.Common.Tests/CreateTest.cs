bfe92e864c60e1824d3e549f312d27d6f59d775e
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2023-02-19 00:50:48 -0500

    Initial tests

---
+++ src/Rxmxnx.PInvoke.Common.Tests/CreateTest.cs
@@ -0,0 +1,62 @@
+﻿namespace Rxmxnx.PInvoke.Tests;
+
+[ExcludeFromCodeCoverage]
+public class IWrapperTests
+{
+    private static readonly IFixture fixture = new Fixture();
+
+    [Fact]
+    internal Task BooleanTestAsync() => TestAsync<Boolean>();
+    [Fact]
+    internal Task ByteTestAsync() => TestAsync<Byte>();
+    [Fact]
+    internal Task Int16TestAsync() => TestAsync<Int16>();
+    [Fact]
+    internal Task CharTestAsync() => TestAsync<Char>();
+    [Fact]
+    internal Task Int32TestAsync() => TestAsync<Int32>();
+    [Fact]
+    internal Task Int64TestAsync() => TestAsync<Int64>();
+    [Fact]
+    internal Task SingleTestAsync() => TestAsync<Single>();
+    [Fact]
+    internal Task DoubleTestAsync() => TestAsync<Double>();
+    [Fact]
+    internal Task DecimalTestAsync() => TestAsync<Decimal>();
+    [Fact]
+    internal Task DateTimeTestAsync() => TestAsync<DateTime>();
+    [Fact]
+    internal Task TimeOnlyTestAsync() => TestAsync<TimeOnly>();
+    [Fact]
+    internal Task TimeSpanTestAsync() => TestAsync<TimeSpan>();
+
+    private static async Task TestAsync<T>() where T : unmanaged
+    {
+        Task inputTest = Task.Run(Input<T>);
+        Task nullTest1 = Task.Run(() => Nullable<T>(false));
+        Task nullTest2 = Task.Run(() => Nullable<T>(true));
+
+        await Task.WhenAll(inputTest, nullTest1, nullTest2);
+    }
+    private static void Input<T>() where T : unmanaged
+    {
+        T value = fixture.Create<T>();
+        T value2 = fixture.Create<T>();
+        var result = IWrapper<T>.Create(value);
+        Assert.NotNull(result);
+        Assert.Equal(value, result.Value);
+        Assert.True(result.Equals(value));
+        Assert.Equal(Equals(value, value2), result.Equals(value2));
+    }
+
+    private static void Nullable<T>(Boolean nullInput) where T : unmanaged
+    {
+        T? value = !nullInput? fixture.Create<T>() : null;
+        T? value2 = fixture.Create<Boolean>() ? fixture.Create<T>() : null;
+        var result = IWrapper<T>.Create(value);
+        Assert.NotNull(result);
+        Assert.Equal(value, result.Value);
+        Assert.Equal(Equals(value, value2), result.Equals(value2));
+    }
+}
+
