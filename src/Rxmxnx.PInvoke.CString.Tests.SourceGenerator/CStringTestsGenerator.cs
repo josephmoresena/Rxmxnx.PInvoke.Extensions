1be4317069d027e90793e27824163305c240c9f8
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2023-03-14 11:53:04 -0500

    Update CStringTestsGenerator.cs

--- src/Rxmxnx.PInvoke.CString.Tests.SourceGenerator/CStringTestsGenerator.cs
+++ src/Rxmxnx.PInvoke.CString.Tests.SourceGenerator/CStringTestsGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
 
@@ -16,14 +17,27 @@ internal static partial class TestSet
 {
     static TestSet()
     {");
+
             strBuild.AppendLine("\t\tutf16Text = new String[] {");
             foreach (String value in StringSet.Set)
                 strBuild.AppendLine($"\t\t\t\"{value}\",");
             strBuild.AppendLine("\t\t};");
+
             strBuild.AppendLine("\t\tutf8Text = new ReadOnlySpanFunc<Byte>[] {");
             foreach (String value in StringSet.Set)
                 strBuild.AppendLine($"\t\t\t() => \"{value}\"u8,");
             strBuild.AppendLine("\t\t};");
+
+            strBuild.AppendLine("\t\tutf8Bytes = new Byte[][] {");
+            foreach (String value in StringSet.Set)
+                strBuild.AppendLine($"\t\t\tnew Byte[] {{ {String.Join(", ", Encoding.UTF8.GetBytes(value).Select(x => $"{x}"))} }},");
+            strBuild.AppendLine("\t\t};");
+
+            strBuild.AppendLine("\t\tutf8NullTerminatedBytes = new Byte[][] {");
+            foreach (String value in StringSet.Set)
+                strBuild.AppendLine($"\t\t\tnew Byte[] {{ {String.Join(", ", Encoding.UTF8.GetBytes(value).Select(x => $"{x}"))}, 0 }},");
+            strBuild.AppendLine("\t\t};");
+
             strBuild.AppendLine(@"    }
 }");
             context.AddSource("TestSet.g.cs", strBuild.ToString());
