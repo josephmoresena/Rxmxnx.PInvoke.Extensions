a75fe7d71dffc64500eb2105b204281c242a4962
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2023-04-21 02:09:59 -0500

    Create CStringDebugView.cs

---
+++ src/Rxmxnx.PInvoke.CString/Internal/Debug/CStringDebugView.cs
@@ -0,0 +1,25 @@
+﻿namespace Rxmxnx.PInvoke.Internal.Debug;
+
+/// <summary>
+/// Debug View class for <see cref="CString"/>
+/// </summary>
+[ExcludeFromCodeCoverage]
+internal sealed record CStringDebugView
+{
+    /// <summary>
+    /// Internal value.
+    /// </summary>
+    private readonly String _value;
+
+    /// <summary>
+    /// Value to display.
+    /// </summary>
+    [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public String Display => this._value;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="cstr">A <see cref="CString"/> instance.</param>
+    public CStringDebugView(CString cstr) => this._value = cstr.ToString();
+}
