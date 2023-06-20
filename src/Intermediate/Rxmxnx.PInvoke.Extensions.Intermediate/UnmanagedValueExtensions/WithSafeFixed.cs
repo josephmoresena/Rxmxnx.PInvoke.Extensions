18811354957f923684a73afb4c92193ffae52937
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2023-06-19 21:14:55 -0500

    Update WithSafeFixed.cs

--- src/Intermediate/Rxmxnx.PInvoke.Extensions.Intermediate/UnmanagedValueExtensions/WithSafeFixed.cs
+++ src/Intermediate/Rxmxnx.PInvoke.Extensions.Intermediate/UnmanagedValueExtensions/WithSafeFixed.cs
@@ -12,7 +12,7 @@ public partial class UnmanagedValueExtensions
     /// <param name="arr">The current array of type <typeparamref name="T"/>.</param>
     /// <param name="action">A delegate of type <see cref="FixedContextAction{T}"/>.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void WithSafeFixed<T>(this T[] arr, FixedContextAction<T> action)
+    public static unsafe void WithSafeFixed<T>(this T[]? arr, FixedContextAction<T> action)
         where T : unmanaged
     {
         ArgumentNullException.ThrowIfNull(action);
@@ -42,7 +42,7 @@ public partial class UnmanagedValueExtensions
     /// <param name="arr">The current array of type <typeparamref name="T"/>.</param>
     /// <param name="action">A delegate of type <see cref="ReadOnlyFixedContextAction{T}"/>.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void WithSafeFixed<T>(this T[] arr, ReadOnlyFixedContextAction<T> action)
+    public static unsafe void WithSafeFixed<T>(this T[]? arr, ReadOnlyFixedContextAction<T> action)
         where T : unmanaged
     {
         ArgumentNullException.ThrowIfNull(action);
@@ -75,7 +75,7 @@ public partial class UnmanagedValueExtensions
     /// <param name="arg">An object representing the state, of type <typeparamref name="TArg"/>.</param>
     /// <param name="action">A delegate of type <see cref="FixedContextAction{T, TArg}"/>.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void WithSafeFixed<T, TArg>(this T[] arr, TArg arg, FixedContextAction<T, TArg> action)
+    public static unsafe void WithSafeFixed<T, TArg>(this T[]? arr, TArg arg, FixedContextAction<T, TArg> action)
         where T : unmanaged
     {
         ArgumentNullException.ThrowIfNull(action);
@@ -107,7 +107,7 @@ public partial class UnmanagedValueExtensions
     /// <param name="arg">An object representing the state, of type <typeparamref name="TArg"/>.</param>
     /// <param name="action">A delegate of type <see cref="ReadOnlyFixedContextAction{T, TArg}"/>.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void WithSafeFixed<T, TArg>(this T[] arr, TArg arg, ReadOnlyFixedContextAction<T, TArg> action)
+    public static unsafe void WithSafeFixed<T, TArg>(this T[]? arr, TArg arg, ReadOnlyFixedContextAction<T, TArg> action)
         where T : unmanaged
     {
         ArgumentNullException.ThrowIfNull(action);
@@ -140,7 +140,7 @@ public partial class UnmanagedValueExtensions
     /// <param name="func">A delegate of type <see cref="FixedContextFunc{T, TResult}"/>.</param>
     /// <returns>The result of executing the function specified by <paramref name="func"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe TResult WithSafeFixed<T, TResult>(this T[] arr, FixedContextFunc<T, TResult> func)
+    public static unsafe TResult WithSafeFixed<T, TResult>(this T[]? arr, FixedContextFunc<T, TResult> func)
         where T : unmanaged
     {
         ArgumentNullException.ThrowIfNull(func);
@@ -172,7 +172,7 @@ public partial class UnmanagedValueExtensions
     /// <param name="func">A delegate of type <see cref="ReadOnlyFixedContextFunc{T, TResult}"/>.</param>
     /// <returns>The result of executing the function specified by <paramref name="func"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe TResult WithSafeFixed<T, TResult>(this T[] arr, ReadOnlyFixedContextFunc<T, TResult> func)
+    public static unsafe TResult WithSafeFixed<T, TResult>(this T[]? arr, ReadOnlyFixedContextFunc<T, TResult> func)
         where T : unmanaged
     {
         ArgumentNullException.ThrowIfNull(func);
@@ -207,7 +207,7 @@ public partial class UnmanagedValueExtensions
     /// <param name="func">A delegate of type <see cref="FixedContextFunc{T, TArg, TResult}"/>.</param>
     /// <returns>The result of executing the function specified by <paramref name="func"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe TResult WithSafeFixed<T, TArg, TResult>(this T[] arr, TArg arg, FixedContextFunc<T, TArg, TResult> func)
+    public static unsafe TResult WithSafeFixed<T, TArg, TResult>(this T[]? arr, TArg arg, FixedContextFunc<T, TArg, TResult> func)
         where T : unmanaged
     {
         ArgumentNullException.ThrowIfNull(func);
@@ -241,7 +241,7 @@ public partial class UnmanagedValueExtensions
     /// <param name="func">A delegate of type <see cref="ReadOnlyFixedContextFunc{T, TArg, TResult}"/>.</param>
     /// <returns>The result of executing the function specified by <paramref name="func"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe TResult WithSafeFixed<T, TArg, TResult>(this T[] arr, TArg arg, ReadOnlyFixedContextFunc<T, TArg, TResult> func)
+    public static unsafe TResult WithSafeFixed<T, TArg, TResult>(this T[]? arr, TArg arg, ReadOnlyFixedContextFunc<T, TArg, TResult> func)
         where T : unmanaged
     {
         ArgumentNullException.ThrowIfNull(func);
