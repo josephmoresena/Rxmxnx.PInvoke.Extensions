27375e22f6fa4aeda4dbc2dfec7142a0b4b08870
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2024-08-19 12:15:24 -0500

    Initial AsMemory

---
+++ src/Intermediate/Rxmxnx.PInvoke.Common.Intermediate/Internal/ArrayMemoryManager.cs
@@ -0,0 +1,53 @@
+namespace Rxmxnx.PInvoke.Internal;
+
+/// <summary>
+/// <see cref="MemoryManager{T}"/> Implementation for abstract array.
+/// </summary>
+/// <typeparam name="T">The type of the array.</typeparam>
+[SuppressMessage(SuppressMessageConstants.CSharpSquid, SuppressMessageConstants.CheckIdS6640)]
+internal sealed class ArrayMemoryManager<T>(Array array) : MemoryManager<T>
+{
+	/// <summary>
+	/// Internal array.
+	/// </summary>
+	private readonly Array _array = array;
+
+	/// <summary>
+	/// <see cref="GCHandle"/> handle.
+	/// </summary>
+	private GCHandle _handle;
+
+	/// <inheritdoc/>
+	public override Span<T> GetSpan()
+	{
+		T managedRef = this.GetArrayDataReference();
+		return MemoryMarshal.CreateSpan(ref managedRef, this._array.Length);
+	}
+	/// <inheritdoc/>
+	public override unsafe MemoryHandle Pin(Int32 elementIndex = 0)
+	{
+		if (this._handle == default)
+			this._handle = GCHandle.Alloc(this._array, GCHandleType.Pinned);
+		ref T managedRef = ref this.GetArrayDataReference();
+		ref T handleRef = ref Unsafe.Add(ref managedRef, elementIndex);
+		return new(Unsafe.AsPointer(ref handleRef), this._handle, this);
+	}
+	/// <inheritdoc/>
+	public override void Unpin()
+	{
+		if (this._handle == default) return;
+		this._handle.Free();
+		this._handle = default;
+	}
+
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	protected override void Dispose(Boolean _) => this.Unpin();
+
+	/// <inheritdoc cref="MemoryMarshal.GetArrayDataReference(Array)"/>
+	private ref T GetArrayDataReference()
+	{
+		ref Byte byteRef = ref MemoryMarshal.GetArrayDataReference(this._array);
+		return ref Unsafe.As<Byte, T>(ref byteRef);
+	}
+}
---
+++ src/Intermediate/Rxmxnx.PInvoke.Extensions.Intermediate/MemoryBlockExtensions/AsMemory.cs
@@ -0,0 +1,76 @@
+namespace Rxmxnx.PInvoke;
+
+public static partial class MemoryBlockExtensions
+{
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,,,,] arr) => new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,,,,,] arr)
+		=> new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,,,,,,] arr)
+		=> new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,,,,,,,] arr)
+		=> new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,,,,,,,,] arr)
+		=> new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,] arr)
+		=> new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,] arr)
+		=> new ArrayMemoryManager<T>(arr).Memory;
+	/// <inheritdoc cref="MemoryExtensions.AsMemory{T}(T[])"/>
+	public static Memory<T> AsMemory<T>(this T[,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,] arr)
+		=> new ArrayMemoryManager<T>(arr).Memory;
+}
