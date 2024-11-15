1050dc1ab723748de3f91653c1b2cf2c665861b1
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2024-11-15 13:11:41 -0500

    Update Program.cs

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Rxmxnx.PInvoke.Buffers;
 
+using PObject = Rxmxnx.PInvoke.Buffers.Primordial<System.Object>;
+using PNBoolean = Rxmxnx.PInvoke.Buffers.Primordial<System.Boolean?>;
+
 public static class Program
 {
 	private static readonly Boolean?[] values =
@@ -8,56 +11,33 @@ public static class Program
 	public static void Main(String[] args)
 	{
 		AllocatedBuffer
-			.Register<Composed<Composed<Primordial<Object>, Primordial<Object>, Object>, Composed<
-				Composed<Composed<Primordial<Object>, Primordial<Object>, Object>,
-					Composed<Primordial<Object>, Primordial<Object>, Object>, Object>,
-				Composed<Composed<Primordial<Object>, Primordial<Object>, Object>,
-					Composed<Primordial<Object>, Primordial<Object>, Object>, Object>, Object>, Object>>();
-		AllocatedBuffer.RegisterNullable<Boolean, Composed<Primordial<Boolean?>, Primordial<Boolean?>, Boolean?>>();
+			.Register<Composed<Composed<PObject, PObject, Object>, Composed<
+					Composed<Composed<PObject, PObject, Object>, Composed<PObject, PObject, Object>, Object>,
+					Composed<Composed<PObject, PObject, Object>, Composed<PObject, PObject, Object>, Object>, Object>,
+				Object>>();
+		AllocatedBuffer
+			.RegisterNullable<Boolean, Composed<Composed<PNBoolean, Composed<PNBoolean, PNBoolean, Boolean?>, Boolean?>,
+				Composed<Composed<PNBoolean, PNBoolean, Boolean?>, Composed<PNBoolean, PNBoolean, Boolean?>, Boolean?>,
+				Boolean?>>();
+
 		Console.Write("Size: ");
 		Int32 count = Int32.Parse(Console.ReadLine()!);
-		AllocatedBuffer.Alloc<String?>((UInt16)count, Program.UseStringSpan);
+		AllocatedBuffer.Alloc<String?>((UInt16)count, Program.StringTest);
 
 		if (!Program.inStack)
 		{
 			Console.WriteLine("Minimal buffer.");
-			AllocatedBuffer.Alloc<String?>((UInt16)count, Program.UseStringSpan, true);
+			AllocatedBuffer.Alloc<String?>((UInt16)count, Program.StringTest, true);
 		}
 
-		AllocatedBuffer.Alloc<Boolean?>((UInt16)Random.Shared.Next(1, Program.values.Length), buff =>
-		{
-			Console.WriteLine("Boolean? Count: " + buff.Span.Length + " In stack: " + buff.InStack + " Allocated: " +
-			                  buff.FullLength);
-			Program.values.AsSpan()[..buff.Span.Length].CopyTo(buff.Span);
-			Console.WriteLine("Begin GC.Collect()");
+		AllocatedBuffer.Alloc<Boolean?>((UInt16)Random.Shared.Next(1, Program.values.Length),
+		                                Program.NullableBooleanTest, true);
 
-			GC.Collect();
-			GC.WaitForFullGCComplete();
-
-			Console.WriteLine("End GC.Collect()");
-
-			Console.WriteLine(Program.values.AsSpan()[..buff.Span.Length].SequenceEqual(buff.Span));
-		});
-
-		Double[] dValues = Enumerable.Range(0, 10).Select(i => Random.Shared.NextDouble()).ToArray();
-		AllocatedBuffer.Alloc((UInt16)Random.Shared.Next(1, dValues.Length), dValues,
-		                      (AllocatedBuffer<Double> buff, in Double[] d) =>
-		                      {
-			                      Console.WriteLine("Double Count: " + buff.Span.Length + " In stack: " + buff.InStack +
-			                                        " Allocated: " + buff.FullLength);
-			                      d.AsSpan()[..buff.Span.Length].CopyTo(buff.Span);
-			                      Console.WriteLine("Begin GC.Collect()");
-
-			                      GC.Collect();
-			                      GC.WaitForFullGCComplete();
-
-			                      Console.WriteLine("End GC.Collect()");
-
-			                      Console.WriteLine(d.AsSpan()[..buff.Span.Length].SequenceEqual(buff.Span));
-		                      });
+		Double[] dValues = Enumerable.Range(0, 10).Select(_ => Random.Shared.NextDouble()).ToArray();
+		AllocatedBuffer.Alloc<Double, Double[]>((UInt16)Random.Shared.Next(1, dValues.Length), dValues,
+		                                        Program.DoubleTest);
 	}
-
-	private static void UseStringSpan(AllocatedBuffer<String?> buff)
+	private static void StringTest(AllocatedBuffer<String?> buff)
 	{
 		Program.inStack = buff.InStack;
 		Console.WriteLine("String? In stack: " + buff.InStack + " Allocated: " + buff.FullLength);
@@ -79,5 +59,34 @@ public static class Program
 	{
 		foreach (ref readonly String? str in span)
 			Console.WriteLine(str);
+	}
+
+	private static void NullableBooleanTest(AllocatedBuffer<Boolean?> buff)
+	{
+		Console.WriteLine("Boolean? Count: " + buff.Span.Length + " In stack: " + buff.InStack + " Allocated: " +
+		                  buff.FullLength);
+		Program.values.AsSpan()[..buff.Span.Length].CopyTo(buff.Span);
+		Console.WriteLine("Begin GC.Collect()");
+
+		GC.Collect();
+		GC.WaitForFullGCComplete();
+
+		Console.WriteLine("End GC.Collect()");
+
+		Console.WriteLine(Program.values.AsSpan()[..buff.Span.Length].SequenceEqual(buff.Span));
+	}
+	private static void DoubleTest(AllocatedBuffer<Double> buff, in Double[] d)
+	{
+		Console.WriteLine("Double Count: " + buff.Span.Length + " In stack: " + buff.InStack + " Allocated: " +
+		                  buff.FullLength);
+		d.AsSpan()[..buff.Span.Length].CopyTo(buff.Span);
+		Console.WriteLine("Begin GC.Collect()");
+
+		GC.Collect();
+		GC.WaitForFullGCComplete();
+
+		Console.WriteLine("End GC.Collect()");
+
+		Console.WriteLine(d.AsSpan()[..buff.Span.Length].SequenceEqual(buff.Span));
 	}
 }
