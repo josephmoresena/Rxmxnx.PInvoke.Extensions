95d21953dbafc76feac54bc1cb29b2d440091d18
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2024-12-15 09:49:27 -0500

    .

--- src/Intermediate/Rxmxnx.PInvoke.Common.Intermediate/IFixedMethod.cs
+++ src/Intermediate/Rxmxnx.PInvoke.Common.Intermediate/IFixedMethod.cs
@@ -1,7 +1,7 @@
 ﻿namespace Rxmxnx.PInvoke;
 
 /// <summary>
-/// Interface representing a method that is fixed in memory.
+/// Interface representing a method whose memory address is fixed in memory.
 /// </summary>
 /// <typeparam name="TDelegate">Type of the delegate that corresponds to the fixed method.</typeparam>
 public interface IFixedMethod<out TDelegate> : IFixedPointer where TDelegate : Delegate
