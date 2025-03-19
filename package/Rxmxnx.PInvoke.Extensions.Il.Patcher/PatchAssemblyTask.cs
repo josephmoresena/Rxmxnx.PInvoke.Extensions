1986d3546f12ed9df6f47dbe669fb03ed1123dc5
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2025-03-19 11:51:29 -0500

    Repair Assembly Strong-Name signature

--- package/Rxmxnx.PInvoke.Extensions.Il.Patcher/PatchAssemblyTask.cs
+++ package/Rxmxnx.PInvoke.Extensions.Il.Patcher/PatchAssemblyTask.cs
@@ -17,10 +17,6 @@ public sealed partial class PatchAssemblyTask : MsBuildTask
 	/// Reader parameters for assembly.
 	/// </summary>
 	private static readonly ReaderParameters readParameters = new() { ReadWrite = true, ReadSymbols = true, };
-	/// <summary>
-	/// Writer parameters for assembly.
-	/// </summary>
-	private static readonly WriterParameters writeParameters = new() { WriteSymbols = true, };
 
 	/// <summary>
 	/// MSBuild Output path.
@@ -30,17 +26,22 @@ public sealed partial class PatchAssemblyTask : MsBuildTask
 	/// Target framework.
 	/// </summary>
 	public String? TargetFramework { get; set; }
+	/// <summary>
+	/// Strong-name Key path.
+	/// </summary>
+	public String? StrongNameKeyPath { get; set; }
 
 	/// <inheritdoc/>
 	public override Boolean Execute()
 	{
 		if (String.IsNullOrEmpty(this.OutputPath) || String.IsNullOrEmpty(this.TargetFramework)) return true;
-		// Runs only if MSBuild sends both parameters.
 
+		// Runs only if MSBuild sends both parameters.
 		DirectoryInfo outputPath = new(this.OutputPath);
 		FileInfo[] assemblyFiles = outputPath.GetFiles(PatchAssemblyTask.AssemblyName, SearchOption.AllDirectories);
 
 		String? assemblyPath = default;
+		String? snkPath = File.Exists(this.StrongNameKeyPath) ? new FileInfo(this.StrongNameKeyPath).FullName : default;
 		try
 		{
 			assemblyPath = assemblyFiles
@@ -49,14 +50,14 @@ public sealed partial class PatchAssemblyTask : MsBuildTask
 			String? documentationPath = outputPath
 			                            .GetFiles(PatchAssemblyTask.AssemblyDocumentationName,
 			                                      SearchOption.AllDirectories).FirstOrDefault()?.FullName;
-			PatchAssemblyTask.PatchAssembly(assemblyPath, documentationPath);
+			PatchAssemblyTask.PatchAssembly(assemblyPath, snkPath, documentationPath);
 			return true;
 		}
 		catch (Exception ex)
 		{
 			this.Log.LogError(
-				$"Error: {ex.Message} Output Path: {outputPath.FullName} Framework: {this.TargetFramework} Count: {assemblyFiles.Length} Assembly: {assemblyPath}");
-			this.Log.LogError(ex.StackTrace);
+				$"Error: {ex.Message} Output Path: {outputPath.FullName} Framework: {this.TargetFramework} SN-Key: {this.StrongNameKeyPath}  Count: {assemblyFiles.Length} Assembly: {assemblyPath} Snk:{snkPath}");
+			this.Log.LogError(ex.ToString());
 			return false;
 		}
 	}
@@ -70,19 +71,24 @@ public sealed partial class PatchAssemblyTask : MsBuildTask
 		if (args.Length < 1)
 			Console.Write("Please select the path of Rxmxnx.PInvoke.Extension assembly.");
 
-		PatchAssemblyTask.PatchAssembly(args[0], args.Length > 1 ? args[1] : default);
+		PatchAssemblyTask.PatchAssembly(args[0], args.Length > 1 ? args[1] : default,
+		                                args.Length > 2 ? args[2] : default);
 	}
 
 	/// <summary>
 	/// Patches the <c>Rxmxnx.PInvoke.Extensions</c> assembly.
 	/// </summary>
 	/// <param name="assemblyPath">Path to <c>Rxmxnx.PInvoke.Extensions</c> assembly.</param>
+	/// <param name="snkPath">Path to <c>Rxmxnx.PInvoke.Extensions</c> strong-name key.</param>
 	/// <param name="documentationPath">Path to <c>Rxmxnx.PInvoke.Extensions</c> documentation.</param>
-	private static void PatchAssembly(String assemblyPath, String? documentationPath = default)
+	private static void PatchAssembly(String assemblyPath, String? snkPath, String? documentationPath = default)
 	{
 		using AssemblyDefinition? assembly =
 			AssemblyDefinition.ReadAssembly(assemblyPath, PatchAssemblyTask.readParameters);
 		using ModuleDefinition? module = assembly.MainModule;
+		WriterParameters writerParameters = !File.Exists(snkPath) ?
+			new() { WriteSymbols = true, } :
+			new() { WriteSymbols = true, StrongNameKeyBlob = File.ReadAllBytes(snkPath), };
 
 		TypeDefinition readOnlyValPtrTypeDefinition =
 			module.Types.First(t => t.Name == PatchAssemblyTask.ReadOnlyValPtrName);
@@ -99,7 +105,7 @@ public sealed partial class PatchAssemblyTask : MsBuildTask
 		                                                       iReadOnlyFixedContext, readOnlyFixedContext);
 		PatchAssemblyTask.ImplementGetUnsafeFixedContextMethod(module, valPtrTypeDefinition, iFixedContext,
 		                                                       fixedContext);
-		assembly.Write(PatchAssemblyTask.writeParameters);
+		assembly.Write(writerParameters);
 
 		if (String.IsNullOrEmpty(documentationPath)) return;
 
