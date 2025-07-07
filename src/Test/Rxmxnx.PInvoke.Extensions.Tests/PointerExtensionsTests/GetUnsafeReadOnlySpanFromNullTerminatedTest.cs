df0a73056baba466c7844077e625cb45a5c1a8dd
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2025-07-07 02:50:07 -0500

    Update GetUnsafeReadOnlySpanFromNullTerminatedTest.cs

--- src/Test/Rxmxnx.PInvoke.Extensions.Tests/PointerExtensionsTests/GetUnsafeReadOnlySpanFromNullTerminatedTest.cs
+++ src/Test/Rxmxnx.PInvoke.Extensions.Tests/PointerExtensionsTests/GetUnsafeReadOnlySpanFromNullTerminatedTest.cs
@@ -12,10 +12,27 @@ public sealed class GetUnsafeReadOnlySpanFromNullTerminatedTest
 		];
 
 	[Fact]
-	internal void Test()
+	internal void CharTest()
+	{
+		ReadOnlyValPtr<Char> charPtr = GetUnsafeReadOnlySpanFromNullTerminatedTest.CharSpan.GetUnsafeValPtr();
+		ReadOnlySpan<Char> charSpan = charPtr.GetUnsafeReadOnlySpanFromNullTerminated();
+
+		Assert.True(Unsafe.AreSame(ref MemoryMarshal.GetReference(GetUnsafeReadOnlySpanFromNullTerminatedTest.CharSpan),
+#if NET8_0_OR_GREATER
+		                           in charPtr.Reference));
+#else
+		                           ref Unsafe.AsRef(in charPtr.Reference)));
+#endif
+		Assert.True(Unsafe.AreSame(ref MemoryMarshal.GetReference(GetUnsafeReadOnlySpanFromNullTerminatedTest.CharSpan),
+		                           ref MemoryMarshal.GetReference(charSpan)));
+		Assert.Equal(GetUnsafeReadOnlySpanFromNullTerminatedTest.CharSpan[..^1].Length, charSpan.Length);
+		Assert.True(charSpan.SequenceEqual(GetUnsafeReadOnlySpanFromNullTerminatedTest.CharSpan[..^1]));
+	}
+	[Fact]
+	internal void ByteTest()
 	{
 		ReadOnlyValPtr<Byte> bytePtr = GetUnsafeReadOnlySpanFromNullTerminatedTest.ByteSpan.GetUnsafeValPtr();
-		ReadOnlyValPtr<Char> charPtr = GetUnsafeReadOnlySpanFromNullTerminatedTest.CharSpan.GetUnsafeValPtr();
+		ReadOnlySpan<Byte> byteSpan = bytePtr.GetUnsafeReadOnlySpanFromNullTerminated();
 
 		Assert.True(Unsafe.AreSame(ref MemoryMarshal.GetReference(GetUnsafeReadOnlySpanFromNullTerminatedTest.ByteSpan),
 #if NET8_0_OR_GREATER
@@ -23,25 +40,9 @@ public sealed class GetUnsafeReadOnlySpanFromNullTerminatedTest
 #else
 		                           ref Unsafe.AsRef(in bytePtr.Reference)));
 #endif
-		Assert.True(Unsafe.AreSame(ref MemoryMarshal.GetReference(GetUnsafeReadOnlySpanFromNullTerminatedTest.CharSpan),
-#if NET8_0_OR_GREATER
-		                           in charPtr.Reference));
-#else
-		                           ref Unsafe.AsRef(in charPtr.Reference)));
-#endif
-
-		ReadOnlySpan<Byte> byteSpan = bytePtr.GetUnsafeReadOnlySpanFromNullTerminated();
-		ReadOnlySpan<Char> charSpan = charPtr.GetUnsafeReadOnlySpanFromNullTerminated();
-
 		Assert.True(Unsafe.AreSame(ref MemoryMarshal.GetReference(GetUnsafeReadOnlySpanFromNullTerminatedTest.ByteSpan),
 		                           ref MemoryMarshal.GetReference(byteSpan)));
-		Assert.True(Unsafe.AreSame(ref MemoryMarshal.GetReference(GetUnsafeReadOnlySpanFromNullTerminatedTest.CharSpan),
-		                           ref MemoryMarshal.GetReference(charSpan)));
-
 		Assert.Equal(GetUnsafeReadOnlySpanFromNullTerminatedTest.ByteSpan.Length, byteSpan.Length);
-		Assert.Equal(GetUnsafeReadOnlySpanFromNullTerminatedTest.CharSpan[..^1].Length, charSpan.Length);
-
 		Assert.True(byteSpan.SequenceEqual(GetUnsafeReadOnlySpanFromNullTerminatedTest.ByteSpan));
-		Assert.True(charSpan.SequenceEqual(GetUnsafeReadOnlySpanFromNullTerminatedTest.CharSpan[..^1]));
 	}
 }
