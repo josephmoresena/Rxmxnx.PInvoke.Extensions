127c975fad0ff8c88ec05b4c2f839da3b8b05dd5
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2025-10-17 12:46:54 -0500

    .

--- src/ApplicationTest/Rxmxnx.PInvoke.LauncherTest/TestCompiler/Private.cs
+++ src/ApplicationTest/Rxmxnx.PInvoke.LauncherTest/TestCompiler/Private.cs
@@ -12,29 +12,36 @@ public partial class TestCompiler
 
 		await TestCompiler.RestoreNet(restoreArgs);
 
-		if (!onlyNativeAot)
+		try
 		{
-			await TestCompiler.CompileNet(compileArgs);
-			compileArgs.BuildDependencies = false;
-
-			if (!OperatingSystem.IsFreeBSD())
+			if (!onlyNativeAot)
 			{
-				compileArgs.Publish = Publish.ReadyToRun;
 				await TestCompiler.CompileNet(compileArgs);
+				compileArgs.BuildDependencies = false;
+
+				if (!OperatingSystem.IsFreeBSD())
+				{
+					compileArgs.Publish = Publish.ReadyToRun;
+					await TestCompiler.CompileNet(compileArgs);
+				}
 			}
+
+			if (!Utilities.IsNativeAotSupported(arch, restoreArgs.Version)) return;
+
+			compileArgs.Publish = Publish.NativeAot;
+			await TestCompiler.CompileNet(compileArgs);
+			await TestCompiler.CompileNetWithSwitches(compileArgs);
+
+			if (!restoreArgs.ProjectFile.EndsWith(".csproj") || restoreArgs.Version > NetVersion.Net90) return;
+			compileArgs.BuildDependencies = false;
+			compileArgs.Publish |= Publish.NoReflection;
+			await TestCompiler.CompileNet(compileArgs);
+			await TestCompiler.CompileNetWithSwitches(compileArgs);
 		}
-
-		if (!Utilities.IsNativeAotSupported(arch, restoreArgs.Version)) return;
-
-		compileArgs.Publish = Publish.NativeAot;
-		await TestCompiler.CompileNet(compileArgs);
-		await TestCompiler.CompileNetWithSwitches(compileArgs);
-
-		if (!restoreArgs.ProjectFile.EndsWith(".csproj") || restoreArgs.Version > NetVersion.Net90) return;
-		compileArgs.BuildDependencies = false;
-		compileArgs.Publish |= Publish.NoReflection;
-		await TestCompiler.CompileNet(compileArgs);
-		await TestCompiler.CompileNetWithSwitches(compileArgs);
+		finally
+		{
+			TestCompiler.NetCleanUp(restoreArgs);
+		}
 	}
 	private static async Task CompileNetWithSwitches(CompileNetArgs compileArgs)
 	{
@@ -87,5 +94,18 @@ public partial class TestCompiler
 			Architecture.Arm or Architecture.Armv6 => OperatingSystem.IsLinux(),
 			_ => !OperatingSystem.IsFreeBSD(),
 		};
+	}
+	private static void NetCleanUp(RestoreNetArgs restoreArgs)
+	{
+		try
+		{
+			DirectoryInfo projectDirectory = new FileInfo(restoreArgs.ProjectFile).Directory!;
+			projectDirectory.GetDirectories("bin", SearchOption.TopDirectoryOnly).FirstOrDefault()?.Delete();
+			projectDirectory.GetDirectories("obj", SearchOption.TopDirectoryOnly).FirstOrDefault()?.Delete();
+		}
+		catch (Exception)
+		{
+			// Ignore
+		}
 	}
 }
