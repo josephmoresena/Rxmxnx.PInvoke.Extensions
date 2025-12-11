e1960f77f766f30a697e2a45412087daa17be034
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2025-12-10 21:39:36 -0500

    Update Builder.cs

--- src/Intermediate/Rxmxnx.PInvoke.CString.Intermediate/CString/Builder.cs
+++ src/Intermediate/Rxmxnx.PInvoke.CString.Intermediate/CString/Builder.cs
@@ -55,7 +55,7 @@ public partial class CString
 		/// A sequence that contains the UTF-8 texts to concatenate and append to the current instance.
 		/// </param>
 		/// <returns>A reference to this instance after the append operation has completed.</returns>
-		public void AppendJoin(ReadOnlySpan<Byte> separator,
+		public Builder AppendJoin(ReadOnlySpan<Byte> separator,
 #if !NET9_0_OR_GREATER
 			params CString?[] values
 #else
@@ -75,7 +75,7 @@ public partial class CString
 		/// A sequence that contains the UTF-8 texts to concatenate and append to the current instance.
 		/// </param>
 		/// <returns>A reference to this instance after the append operation has completed.</returns>
-		public void AppendJoin(ReadOnlySpan<Byte> separator,
+		public Builder AppendJoin(ReadOnlySpan<Byte> separator,
 #if NET9_0_OR_GREATER
 			params ReadOnlySpan<CString?> values
 #else
@@ -84,7 +84,7 @@ public partial class CString
 		)
 		{
 			ReadOnlySpan<CString?>.Enumerator enumerator = values.GetEnumerator();
-			if (!enumerator.MoveNext()) return;
+			if (!enumerator.MoveNext()) return this;
 #if NET9_0_OR_GREATER
 			using (this._lock.EnterScope())
 #else
@@ -98,6 +98,7 @@ public partial class CString
 					this._chunk = this._chunk.Append(enumerator.Current);
 				}
 			}
+			return this;
 		}
 		/// <summary>
 		/// Concatenates the UTF-8 texts of the provided sequence, using the specified UTF-8 sequence separator between
@@ -111,15 +112,28 @@ public partial class CString
 		/// A sequence that contains the UTF-8 texts to concatenate and append to the current instance.
 		/// </param>
 		/// <returns>A reference to this instance after the append operation has completed.</returns>
-		public void AppendJoin(ReadOnlySpan<Byte> separator, CStringSequence sequence)
+		public Builder AppendJoin(ReadOnlySpan<Byte> separator, CStringSequence sequence)
+			=> this.AppendJoin(separator, new CStringSequence.Utf8View(sequence, true));
+		/// <summary>
+		/// Concatenates the UTF-8 texts of the provided sequence, using the specified UTF-8 sequence separator between
+		/// each text, then appends the result to the current instance.
+		/// </summary>
+		/// <param name="separator">
+		/// The UTF-8 sequence to use as a separator. <paramref name="separator"/> is included in the joined text
+		/// only if <paramref name="sequenceView"/> has more than one element.
+		/// </param>
+		/// <param name="sequenceView">
+		/// A sequence that contains the UTF-8 texts to concatenate and append to the current instance.
+		/// </param>
+		/// <returns>A reference to this instance after the append operation has completed.</returns>
+		public Builder AppendJoin(ReadOnlySpan<Byte> separator, CStringSequence.Utf8View sequenceView)
 		{
-			CStringSequence.Utf8View view = new(sequence, true);
 #if NET9_0_OR_GREATER
-			using CStringSequence.Utf8View.Enumerator enumerator = view.GetEnumerator();
+			using CStringSequence.Utf8View.Enumerator enumerator = sequenceView.GetEnumerator();
 #else
-			CStringSequence.Utf8View.Enumerator enumerator = view.GetEnumerator();
+			CStringSequence.Utf8View.Enumerator enumerator = sequenceView.GetEnumerator();
 #endif
-			if (!enumerator.MoveNext()) return;
+			if (!enumerator.MoveNext()) return this;
 #if NET9_0_OR_GREATER
 			using (this._lock.EnterScope())
 #else
@@ -133,12 +147,14 @@ public partial class CString
 					this._chunk = this._chunk.Append(enumerator.Current);
 				}
 			}
+			return this;
 		}
 		/// <summary>
 		/// Appends each non-empty UTF-8 text in <paramref name="sequence"/>.
 		/// </summary>
 		/// <param name="sequence">A UTF-8 text sequence to append.</param>
-		public void Append(CStringSequence sequence)
+		/// <returns>A reference to this instance after the append operation has completed.</returns>
+		public Builder Append(CStringSequence sequence)
 		{
 			CStringSequence.Utf8View view = new(sequence, false);
 #if NET9_0_OR_GREATER
@@ -150,22 +166,26 @@ public partial class CString
 				foreach (ReadOnlySpan<Byte> value in view)
 					this._chunk = this._chunk.Append(value);
 			}
+			return this;
 		}
 		/// <summary>
 		/// Appends a UTF-8 text to this instance.
 		/// </summary>
 		/// <param name="value">The read-only byte span to append.</param>
-		public void Append(CString value) => this.Append(value.AsSpan());
+		/// <returns>A reference to this instance after the append operation has completed.</returns>
+		public Builder Append(CString value) => this.Append(value.AsSpan());
 		/// <summary>
 		/// Appends the UTF-8 representation of <paramref name="value"/>.
 		/// </summary>
 		/// <param name="value">The read-only character span to append.</param>
-		public void Append(String value) => this.Append(value.AsSpan());
+		/// <returns>A reference to this instance after the append operation has completed.</returns>
+		public Builder Append(String value) => this.Append(value.AsSpan());
 		/// <summary>
 		/// Appends the UTF-8 representation of a specified read-only byte span to this instance.
 		/// </summary>
 		/// <param name="value">The read-only byte span to append.</param>
-		public void Append(ReadOnlySpan<Byte> value)
+		/// <returns>A reference to this instance after the append operation has completed.</returns>
+		public Builder Append(ReadOnlySpan<Byte> value)
 		{
 #if NET9_0_OR_GREATER
 			using (this._lock.EnterScope())
@@ -173,12 +193,14 @@ public partial class CString
 			lock (this._lock)
 #endif
 				this._chunk = this._chunk.Append(value);
+			return this;
 		}
 		/// <summary>
 		/// Appends the UTF-8 representation of a specified read-only character span to this instance.
 		/// </summary>
 		/// <param name="value">The read-only character span to append.</param>
-		public void Append(ReadOnlySpan<Char> value)
+		/// <returns>A reference to this instance after the append operation has completed.</returns>
+		public Builder Append(ReadOnlySpan<Char> value)
 		{
 #if NET9_0_OR_GREATER
 			using (this._lock.EnterScope())
@@ -186,12 +208,13 @@ public partial class CString
 			lock (this._lock)
 #endif
 				this._chunk = this._chunk.Append(Encoding.UTF8.GetByteCount(value), value);
+			return this;
 		}
 		/// <summary>
 		/// Appends the default line terminator to the end of the current instance.
 		/// </summary>
 		/// <returns>A reference to this instance after the append operation has completed.</returns>
-		public void AppendLine()
+		public Builder AppendLine()
 		{
 #if NET9_0_OR_GREATER
 			using (this._lock.EnterScope())
@@ -199,11 +222,32 @@ public partial class CString
 			lock (this._lock)
 #endif
 				this._chunk = this._chunk.Append(CString.NewLine);
+			return this;
+		}
+		/// <summary>
+		/// Appends the UTF-8 representation of a specified string followed by the default line terminator to the end of
+		/// the current instance.
+		/// </summary>
+		/// <param name="value">The string to append.</param>
+		/// <returns>A reference to this instance after the append operation has completed.</returns>
+		public Builder AppendLine(String? value)
+		{
+#if NET9_0_OR_GREATER
+			using (this._lock.EnterScope())
+#else
+			lock (this._lock)
+#endif
+			{
+				ReadOnlySpan<Char> chars = value;
+				this._chunk = this._chunk.Append(Encoding.UTF8.GetByteCount(chars), chars).Append(CString.NewLine);
+			}
+			return this;
 		}
 		/// <summary>
 		/// Removes all units from the current <see cref="Builder"/> instance.
 		/// </summary>
-		public void Clear()
+		/// <returns>A reference to this instance after the append operation has completed.</returns>
+		public Builder Clear()
 		{
 #if NET9_0_OR_GREATER
 			using (this._lock.EnterScope())
@@ -211,6 +255,7 @@ public partial class CString
 			lock (this._lock)
 #endif
 				this._chunk.Reset(this._capacity);
+			return this;
 		}
 	}
 }
