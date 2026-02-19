d77b72c78dc8d8ac04b64dc93033b07e0af35206
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2026-02-19 17:48:44 -0500

    Update Private.cs

--- src/Intermediate/Rxmxnx.PInvoke.Common.Intermediate/AotInfo/Private.cs
+++ src/Intermediate/Rxmxnx.PInvoke.Common.Intermediate/AotInfo/Private.cs
@@ -6,120 +6,6 @@
 #endif
 public static partial class AotInfo
 {
-#if !NET6_0_OR_GREATER
-	/// <summary>
-	/// Indicates whether JIT is enabled in the current runtime.
-	/// </summary>
-	/// <returns>
-	/// <see langword="true"/> if Jit is enabled; otherwise, <see langword="false"/>.
-	/// </returns>
-	[UnconditionalSuppressMessage("Trimming", "IL2070")]
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	private static Boolean IsJitEnabled()
-	{
-		Boolean hasReflectionEmit = false;
-		try
-		{
-			if (!TrimInfo.StringTypeNameContainsString())
-			{
-				// If reflection disabled, is AOT.
-				AotInfo.reflectionDisabled = true;
-				return false;
-			}
-
-			if (typeof(RuntimeInformation).Assembly.GetType("System.Runtime.JitInfo") is { } typeJitInfo)
-			{
-				// Tries to retrieve JIT information using reflection.
-				Boolean? isJitEnabled = AotInfo.IsJitEnabled(typeJitInfo);
-				if (isJitEnabled.HasValue && isJitEnabled.Value)
-					return true;
-			}
-
-			foreach (Assembly assembly in AotInfo.GetAssembliesSpan())
-			{
-				if (String.IsNullOrEmpty(assembly.FullName)) continue;
-				if (assembly.FullName.StartsWith("System.Reflection.Emit"))
-				{
-					hasReflectionEmit = true;
-					continue;
-				}
-				switch (assembly.FullName[..assembly.FullName.IndexOf(',')])
-				{
-					case "System.Reflection.Emit":
-						hasReflectionEmit = true;
-						continue;
-					case "Microsoft.iOS":
-					case "Xamarin.iOS":
-						return false;
-				}
-			}
-
-			if (MemoryInspector.IsSupported && MonoInfo.MonoAssemblyNameType is not null && AotInfo.IsAotFrame())
-				return true;
-		}
-		// If exception, might be AOT.
-		catch (Exception)
-		{
-			return false;
-		}
-
-		// System.Reflection.Emit is not allowed in AOT/IL2CPP.
-		return hasReflectionEmit && EmitInfo.IsEmitAllowed;
-	}
-	/// <summary>
-	/// Indicates whether JIT is enabled in the current runtime using reflection.
-	/// </summary>
-	/// <param name="typeJitInfo">CLR type of System.Runtime.JitInfo class.</param>
-	/// <returns>
-	/// <see langword="true"/> if Jit is enabled, <see langword="false"/> if Jit is disabled;
-	/// otherwise, <see langword="null"/>.
-	/// </returns>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	[UnconditionalSuppressMessage("AOT", "IL3050")]
-	[UnconditionalSuppressMessage("Trimming", "IL2070")]
-	private static Boolean? IsJitEnabled(Type typeJitInfo)
-	{
-		const BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
-
-#if !NET5_0_OR_GREATER
-		Type typeofFunc = typeof(Func<Boolean, Int64>);
-#endif
-		Func<Boolean, Int64>? getCompiledIlBytes = typeJitInfo.GetMethod("GetCompiledILBytes", bindingFlags)
-#if !NET5_0_OR_GREATER
-		                                                      ?.CreateDelegate(typeofFunc) as Func<Boolean, Int64>;
-#else
-		                                                      ?.CreateDelegate<Func<Boolean, Int64>>();
-#endif
-		Int64? reflectionBytes = getCompiledIlBytes?.Invoke(false);
-
-		if (reflectionBytes.GetValueOrDefault() != 0L)
-			return true;
-
-		Func<Boolean, Int64>? getCompiledMethodCount = typeJitInfo.GetMethod("GetCompiledMethodCount", bindingFlags)
-#if !NET5_0_OR_GREATER
-		                                                          ?.CreateDelegate(typeofFunc) as Func<Boolean, Int64>;
-#else
-		                                                          ?.CreateDelegate<Func<Boolean, Int64>>();
-#endif
-		Int64? methodCount = getCompiledMethodCount?.Invoke(false);
-
-		if (methodCount.GetValueOrDefault() != 0L)
-			return true;
-
-		if (reflectionBytes.HasValue || methodCount.HasValue)
-			return false;
-
-		return default; // Unabled to retrieve JIT information.
-	}
-	/// <inheritdoc cref="AppDomain.GetAssemblies()"/>
-	/// <returns>A read-only span of assemblies in this application domain.</returns>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	private static ReadOnlySpan<Assembly> GetAssembliesSpan()
-	{
-		Assembly[] array = AppDomain.CurrentDomain.GetAssemblies();
-		return MemoryMarshal.CreateReadOnlySpan(ref NativeUtilities.GetArrayDataReference(array), array.Length);
-	}
-#endif
 	/// <summary>
 	/// Indicates whether the executing frame is AOT.
 	/// </summary>
@@ -155,4 +41,114 @@ public static partial class AotInfo
 #endif
 	private static unsafe Boolean IsNativeMethod(RuntimeMethodHandle methodHandle)
 		=> MemoryInspector.Instance.IsLiteral(methodHandle.GetFunctionPointer().ToPointer());
+#if !NET6_0_OR_GREATER
+	/// <summary>
+	/// Indicates whether JIT is enabled in the current runtime.
+	/// </summary>
+	/// <returns>
+	/// <see langword="true"/> if Jit is enabled; otherwise, <see langword="false"/>.
+	/// </returns>
+	[UnconditionalSuppressMessage("Trimming", "IL2070")]
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private static Boolean IsJitEnabled()
+	{
+		Boolean hasReflectionEmit = false;
+		try
+		{
+			if (!TrimInfo.StringTypeNameContainsString())
+			{
+				// If reflection disabled, is AOT.
+				AotInfo.reflectionDisabled = true;
+				return false;
+			}
+
+			foreach (Assembly assembly in AotInfo.GetAssembliesSpan())
+			{
+				if (String.IsNullOrEmpty(assembly.FullName)) continue;
+				switch (assembly.FullName[..assembly.FullName.IndexOf(',')])
+				{
+					case "System.Reflection.Emit":
+						hasReflectionEmit = true;
+						continue;
+					case "Microsoft.iOS":
+					case "Xamarin.iOS":
+						return false;
+				}
+			}
+
+			if (MonoInfo.MonoAssemblyNameType is not null && MemoryInspector.IsSupported)
+				return AotInfo.IsAotFrame();
+
+			if (MonoInfo.MonoAssemblyNameType is null &&
+			    typeof(RuntimeInformation).Assembly.GetType("System.Runtime.JitInfo") is { } jitInfoType)
+			{
+				// Tries to retrieve JIT information using reflection.
+				Boolean? isJitEnabled = AotInfo.IsJitEnabled(jitInfoType);
+				if (isJitEnabled.HasValue && isJitEnabled.Value)
+					return true;
+			}
+		}
+		// If exception, might be AOT.
+		catch (Exception)
+		{
+			return false;
+		}
+
+		// System.Reflection.Emit is not allowed in AOT/IL2CPP.
+		return hasReflectionEmit && EmitInfo.IsEmitAllowed;
+	}
+	/// <summary>
+	/// Indicates whether JIT is enabled in the current runtime using reflection.
+	/// </summary>
+	/// <param name="jitInfoType">CLR type of System.Runtime.JitInfo class.</param>
+	/// <returns>
+	/// <see langword="true"/> if Jit is enabled, <see langword="false"/> if Jit is disabled;
+	/// otherwise, <see langword="null"/>.
+	/// </returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	[UnconditionalSuppressMessage("AOT", "IL3050")]
+	[UnconditionalSuppressMessage("Trimming", "IL2070")]
+	private static Boolean? IsJitEnabled(Type jitInfoType)
+	{
+		const BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+
+#if !NET5_0_OR_GREATER
+		Type typeofFunc = typeof(Func<Boolean, Int64>);
+#endif
+		Func<Boolean, Int64>? getCompiledIlBytes = jitInfoType.GetMethod("GetCompiledILBytes", bindingFlags)
+#if !NET5_0_OR_GREATER
+		                                                      ?.CreateDelegate(typeofFunc) as Func<Boolean, Int64>;
+#else
+		                                                      ?.CreateDelegate<Func<Boolean, Int64>>();
+#endif
+		Int64? reflectionBytes = getCompiledIlBytes?.Invoke(false);
+
+		if (reflectionBytes.GetValueOrDefault() != 0L)
+			return true;
+
+		Func<Boolean, Int64>? getCompiledMethodCount = jitInfoType.GetMethod("GetCompiledMethodCount", bindingFlags)
+#if !NET5_0_OR_GREATER
+		                                                          ?.CreateDelegate(typeofFunc) as Func<Boolean, Int64>;
+#else
+		                                                          ?.CreateDelegate<Func<Boolean, Int64>>();
+#endif
+		Int64? methodCount = getCompiledMethodCount?.Invoke(false);
+
+		if (methodCount.GetValueOrDefault() != 0L)
+			return true;
+
+		if (reflectionBytes.HasValue || methodCount.HasValue)
+			return false;
+
+		return default; // Unabled to retrieve JIT information.
+	}
+	/// <inheritdoc cref="AppDomain.GetAssemblies()"/>
+	/// <returns>A read-only span of assemblies in this application domain.</returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private static ReadOnlySpan<Assembly> GetAssembliesSpan()
+	{
+		Assembly[] array = AppDomain.CurrentDomain.GetAssemblies();
+		return MemoryMarshal.CreateReadOnlySpan(ref NativeUtilities.GetArrayDataReference(array), array.Length);
+	}
+#endif
 }
