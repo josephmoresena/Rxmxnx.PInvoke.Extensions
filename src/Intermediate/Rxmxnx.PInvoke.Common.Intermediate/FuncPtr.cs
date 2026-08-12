c5931eeaec3665ca53e91c8320d219d427ef6d95
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2026-08-11 20:07:54 -0500

    Update FuncPtr.cs

--- src/Intermediate/Rxmxnx.PInvoke.Common.Intermediate/FuncPtr.cs
+++ src/Intermediate/Rxmxnx.PInvoke.Common.Intermediate/FuncPtr.cs
@@ -70,6 +70,9 @@ public readonly unsafe partial struct FuncPtr<TDelegate> : IWrapper<IntPtr>, IEq
 
 	IntPtr IWrapper<IntPtr>.Value => this.Pointer;
 #if NETSTANDARD2_1 || NETCOREAPP3_0_OR_GREATER
+#if !PACKAGE
+	[ExcludeFromCodeCoverage]
+#endif
 	IntPtr IWrapper.IBase<IntPtr>.Value => this.Pointer;
 #endif
 
