00bcaf92046ab507cfbe63f55f83b5eb7d4521d6
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2026-08-12 12:52:23 -0500

    Update FixedPointerValueExtensions.cs

--- src/Intermediate/Rxmxnx.PInvoke.Extensions.Intermediate/FixedPointerValueExtensions.cs
+++ src/Intermediate/Rxmxnx.PInvoke.Extensions.Intermediate/FixedPointerValueExtensions.cs
@@ -121,6 +121,9 @@ public static unsafe class FixedPointerValueExtensions
 	/// <typeparam name="TAction">Type of <see cref="IFixedAction"/>.</typeparam>
 	/// <param name="span">The current span of type <typeparamref name="T"/>.</param>
 	/// <param name="action">A <typeparamref name="TAction"/> instance.</param>
+#if !PACKAGE
+	[ExcludeFromCodeCoverage]
+#endif
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void WithSafeFixed<T, TAction>(this Span<T> span, ref TAction action)
 #if !NET9_0_OR_GREATER
@@ -145,6 +148,9 @@ public static unsafe class FixedPointerValueExtensions
 	/// <typeparam name="TAction">Type of <see cref="IFixedAction"/>.</typeparam>
 	/// <param name="span">The current read-only span of type <typeparamref name="T"/>.</param>
 	/// <param name="action">A <typeparamref name="TAction"/> instance.</param>
+#if !PACKAGE
+	[ExcludeFromCodeCoverage]
+#endif
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void WithSafeFixed<T, TAction>(this ReadOnlySpan<T> span, ref TAction action)
 #if !NET9_0_OR_GREATER
@@ -238,6 +244,9 @@ public static unsafe class FixedPointerValueExtensions
 	/// <param name="span">The current span of type <typeparamref name="T"/>.</param>
 	/// <param name="func">A <typeparamref name="TFunction"/> instance.</param>
 	/// <param name="result">Output. Function result.</param>
+#if !PACKAGE
+	[ExcludeFromCodeCoverage]
+#endif
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void WithSafeFixed<T, TResult, TFunction>(this Span<T> span, ref TFunction func, out TResult result)
 #if !NET9_0_OR_GREATER
@@ -267,6 +276,9 @@ public static unsafe class FixedPointerValueExtensions
 	/// <param name="span">The current read-only span of type <typeparamref name="T"/>.</param>
 	/// <param name="func">A <typeparamref name="TFunction"/> instance.</param>
 	/// <param name="result">Output. Function result.</param>
+#if !PACKAGE
+	[ExcludeFromCodeCoverage]
+#endif
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void WithSafeFixed<T, TResult, TFunction>(this ReadOnlySpan<T> span, ref TFunction func,
 		out TResult result)
